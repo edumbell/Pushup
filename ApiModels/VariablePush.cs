409d4c81d429dd815e12e55c6f6b6d3f5b3844a1
Ed <no@no.no>
2016-12-13 07:33:28 +0200

    adding working version

---
+++ ApiModels/VariablePush.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pushup.ApiModels
+{
+    public class VariableDataPush
+    {
+        public string name { get; set; }
+        public decimal value { get; set; }
+
+    }
+
+    public class VariableConfigPush
+    {
+        public VariableConfigPush()
+        {
+            summaryDisplayCount = 10;
+        }
+
+        public string name { get; set; }
+
+        // amount of time that will be "chunked" into a min/max/median history
+        // = amount of raw data displayed
+        public int summaryGranularityMinutes { get; set; }
+        // how many history summaries to display for this variable.
+        // (SummaryGranularityMinutes * SummaryDisplayCount = total minutes of history shown
+        public int summaryDisplayCount { get; set; }
+
+        // todo:
+        public bool displayGraph { get; set; }
+
+    }
+}
