c7d7a9524e389dc85bf5382f91ae5af5b1be56c4
Ed <no@no.no>
2017-01-12 17:50:21 +0200

    keep alive; fix fakepush url

--- Pushup/Global.asax.cs
+++ Pushup/Global.asax.cs
@@ -6,12 +6,18 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Http;
+using System.Net;
+using System.Threading;
 
 
 namespace Pushup
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+
+        private static string FirstUrl { get; set; }
+  
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
@@ -19,7 +25,26 @@ namespace Pushup
             GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            if (FirstUrl == null)
+            {
+                FirstUrl = HttpContext.Current.Request.Url.ToString();
+                KeepAlive();
+            }
+        }
+
+        public void KeepAlive()
+        {
+            var client = new WebClient();
+            var content = client.DownloadString(FirstUrl);
+            var timer = new System.Threading.Timer(x =>
+            {
+                this.KeepAlive();
+            }, null, new TimeSpan(0,10,0), Timeout.InfiniteTimeSpan);
         }
     }
 }
